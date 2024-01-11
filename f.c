9ab587593a0fcab7f6a1d1383135df94f1e3a6b1
githubdebaby <2151341908@qq.com>
2024-01-11 18:43:21 +0800

    f.c提交

---
+++ f.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main(void)
+{
+	printf("first1");
+	
+	printf("first1");}
+	printf("first1");}
+	printf("first1");}
+	printf("first1");}
+	printf("first1");}
+	printf("first1");}
+	printf("first1");}
+}
+
