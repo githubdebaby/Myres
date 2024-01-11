d050a6e873962086979f2bc606ca74cf0d8d0f19
githubdebaby <156167341+githubdebaby@users.noreply.github.com>
2024-01-11 18:45:16 +0800

    Update f.c

--- f.c
+++ f.c
@@ -3,14 +3,7 @@
 #include<stdlib.h>
 int main(void)
 {
-	printf("first1");
 	
-	printf("first1");}
-	printf("first1");}
-	printf("first1");}
-	printf("first1");}
-	printf("first1");}
-	printf("first1");}
-	printf("first1");}
+	printf("给爷死");}
 }
 
